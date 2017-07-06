720ad04a6961748ebe99b2c85159303f5720e34f
mozhulee <21621232@zju.edu.cn>
2017-07-07 00:07:21 +0800

    Add kubestack cni plugin
    Change-Id: I2942f2d8ea8894d767349ff6237eb83d89714430
    Implements: blueprint cni-plugin
    Signed-off-by: mozhuli <21621232@zju.edu.cn>

---
+++ pkg/kubestack/plugins/plugins.go
@@ -0,0 +1,73 @@
+package plugins
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/containernetworking/cni/pkg/types/current"
+	"github.com/golang/glog"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/ports"
+)
+
+type PluginInterface interface {
+	SetupInterface(podName, podInfraContainerID string, port *ports.Port, ipcidr, gateway, ifName string) (*current.Interface, *current.Interface, error)
+	DestroyInterface(podName, podInfraContainerID string, port *ports.Port) error
+	Init(integrationBridge string) error
+}
+
+// Factory is a function that returns a networkplugin.Interface.
+// The config parameter provides an io.Reader handler to the factory in
+// order to load specific configurations. If no configuration is provided
+// the parameter is nil.
+type Factory func() (PluginInterface, error)
+
+// All registered network plugins.
+var pluginsMutex sync.Mutex
+var plugins = make(map[string]Factory)
+
+// RegisterNetworkPlugin registers a networkplugin.Factory by name.  This
+// is expected to happen during app startup.
+func RegisterNetworkPlugin(name string, networkPlugin Factory) {
+	pluginsMutex.Lock()
+	defer pluginsMutex.Unlock()
+	if _, found := plugins[name]; found {
+		glog.Fatalf("Network plugin %q was registered twice", name)
+	}
+	glog.V(1).Infof("Registered network plugin %q", name)
+	plugins[name] = networkPlugin
+}
+
+// GetNetworkPlugin creates an instance of the named network plugin, or nil if
+// the name is not known.  The error return is only used if the named plugin
+// was known but failed to initialize.
+func GetNetworkPlugin(name string) (PluginInterface, error) {
+	pluginsMutex.Lock()
+	defer pluginsMutex.Unlock()
+	f, found := plugins[name]
+	if !found {
+		return nil, nil
+	}
+	return f()
+}
+
+// InitNetworkPlugin creates an instance of the named networkPlugin plugin.
+func InitNetworkPlugin(name string) (PluginInterface, error) {
+	var networkPlugin PluginInterface
+
+	if name == "" {
+		glog.Info("No network plugin specified.")
+		return nil, nil
+	}
+
+	var err error
+	networkPlugin, err = GetNetworkPlugin(name)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not init networkPlugin plugin %q: %v", name, err)
+	}
+	if networkPlugin == nil {
+		return nil, fmt.Errorf("unknown networkPlugin plugin %q", name)
+	}
+
+	return networkPlugin, nil
+}
---
+++ pkg/kubestack/types/types.go
@@ -0,0 +1,21 @@
+package types
+
+import (
+	"net"
+
+	"github.com/containernetworking/cni/pkg/types"
+)
+
+type NetConf struct {
+	types.NetConf
+	KubestackConfig string `json:"kubestack-config"`
+}
+
+// K8sArgs is the valid CNI_ARGS used for Kubernetes
+type K8sArgs struct {
+	types.CommonArgs
+	IP                         net.IP
+	K8S_POD_NAME               types.UnmarshallableString
+	K8S_POD_NAMESPACE          types.UnmarshallableString
+	K8S_POD_INFRA_CONTAINER_ID types.UnmarshallableString
+}
